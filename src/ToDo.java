626560621af6b1bb1ec801d92588e7d890983737
merlinbergmann <hello@merlin.codes>
2017-12-14 11:43:08 +0100

    feat(ToDo): Print Usage done

--- src/ToDo.java
+++ src/ToDo.java
@@ -1,6 +1,14 @@
 public class ToDo {
 
   public static void main(String[] args) {
-    
+
+    System.out.println("Command Line Todo application");
+    System.out.println("=============================" + "\n");
+    System.out.println("Command line arguments:");
+    System.out.println("-l" + "\t" + "Lists all tasks");
+    System.out.println("-a" + "\t" + "Adds a new task");
+    System.out.println("-r" + "\t" + "Removes a task");
+    System.out.println("-c" + "\t" + "Completes a task");
+
   }
 }
